1b65483bf66b323345b836a2f0aa8a19021585f4
Sijan-Giri <girisijan346@gmail.com>
2024-11-20 09:32:43 +0545

    Learning socket.io connection , take data , send data

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+const {Server} = require("socket.io");
 
-app.listen(3000,() => {
+const server = app.listen(3000,() => {
     console.log("Server started at port 3000...")
+})
+
+const io = new Server(server)
+
+io.on("connection",(socket) => {
+
+    socket.emit("hello",{
+        "greeting" : "Good Morning!"
+    })
+    // console.log("Someone has connected");
+    // socket.on("disconnect",()=> {
+    //     console.log("User is disconnected")
+    // })
+    // socket.on("sendData",(data) => {
+    //     console.log(data)
+    // })
 })
