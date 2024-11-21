23857fcfc1dc32913d9980511a52664d820b3c75
Sijan-Giri <girisijan346@gmail.com>
2024-11-21 17:28:37 +0545

    get , update , delete Socket done

--- app.js
+++ app.js
@@ -14,18 +14,59 @@ const io = new Server(server)
 
 io.on("connection",(socket) => {
     socket.on("addBook",async(data) => {
-        if(data) {
-            const {bookName , description , bookPrice} = data;
-            const book = await Book.create({
-                bookName,
-                description,
-                bookPrice
-            })
-            socket.emit({
-                status : 200,
-                message : 'Book created successfully',
-                data : book
-            })
+        try {
+            if(data) {
+                const {bookName , description , bookPrice} = data;
+                const book = await Book.create({
+                    bookName,
+                    description,
+                    bookPrice
+                })
+                socket.emit("response",{
+                    status : 200,
+                    message : 'Book created successfully',
+                    data : book
+                })
+            }
+        } catch (error) {
+            socket.emit("response",{status : 400 , message : "Something went wrong"})
+        }
+    })
+
+    socket.on("getBooks",async() => {
+         try {
+            const book = await Book.find();
+            socket.emit("response",{status : 200,message : "Books fetched successfully",data : book})
+         } catch (error) {
+            socket.emit("response",{status : 400 , message : "Something went wrong"})
+         }
+    })
+
+    socket.on("updateBook",async(data) => {
+        try {
+            if(data) {
+                const {bookName , description , bookPrice , bookId} = data
+                const updatedBook = await Book.findByIdAndUpdate(bookId,{
+                    bookName,
+                    description,
+                    bookPrice
+            },{new:true});
+                socket.emit("response",{status : 200, message : "Book updated successfully",data : updatedBook})
+            }
+        } catch (error) {
+            socket.emit("response",{status : 400 , message : "Something went wrong"})
+        }
+    })
+
+    socket.on("deleteBook",async(data) => {
+        try {
+            if(data) {
+                const {bookId} = data;
+                await Book.findByIdAndDelete(bookId);
+                socket.emit("response",{status : 200 , message : "Book deleted successfully"})
+            }
+        } catch (error) {
+            socket.emit("response",{status : 400 , message : "Something went wrong!!"})
         }
     })
 })
